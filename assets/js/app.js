b82f0dfbed25934dc3394d2cfb41bef1f70ed022
Suresh Pawar <sureshpawar270@gmail.com>
2023-06-08 16:12:30 +0530

    Card Task commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,99 +1,52 @@
 var cl = console.log ;  //first class function(FCF)
 
-const postsContainer = document.getElementById("postsContainer")
+const imgContainer = document.getElementById("imgContainer");
 
-
-let posts = [
-    {
-      userId: 1,
-      id: 1,
-      title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-      body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-    },
-    {
-      userId: 1,
-      id: 2,
-      title: "qui est esse",
-      body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
-    },
-    {
-      userId: 1,
-      id: 3,
-      title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-      body: "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
-    },
-    {
-      userId: 1,
-      id: 4,
-      title: "eum et est occaecati",
-      body: "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
-    },
-    {
-      userId: 1,
-      id: 5,
-      title: "nesciunt quas odio",
-      body: "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque"
-    },
-    {
-      userId: 1,
-      id: 6,
-      title: "dolorem eum magni eos aperiam quia",
-      body: "ut aspernatur corporis harum nihil quis provident sequi\nmollitia nobis aliquid molestiae\nperspiciatis et ea nemo ab reprehenderit accusantium quas\nvoluptate dolores velit et doloremque molestiae"
-    }
-  ]
-
+const  images = [
+  `https://source.unsplash.com/random/?city`,
+  `https://source.unsplash.com/random/?nature`,
+  `https://source.unsplash.com/random/?javascript`,
+  `https://source.unsplash.com/random/?fashion`,
+  `https://source.unsplash.com/random/?flower`,
+  `https://source.unsplash.com/random/?birds`,
+  `https://source.unsplash.com/random/?angular`,
+  `https://source.unsplash.com/random/?html`,
+  `https://source.unsplash.com/random/?css`,
+  `https://source.unsplash.com/random/?node`,
+  `https://source.unsplash.com/random/?sea`,
+  `https://source.unsplash.com/random/?animal`,
+  `https://source.unsplash.com/random/?farm`,
+  `https://source.unsplash.com/random/?model`,
+  `https://source.unsplash.com/random/?sky`,
+]
 
 let result = " ";
-// forEach without using Arrow function 
 
-// posts.forEach(function(post){
-//     result +=`
-//                 <div class="col-md-4 mb-3" >
-//                     <div class="card">
-//                     <div class="card-header">
-//                         <h3>${post.title}</h3>
-//                     </div>
-//                     <div class="card-body text-right">
-//                         <p>${post.body}</p>
-//                     </div>
-//                     <div class="card-footer ">
-//                         <button class="btn btn-primary">Edit</button>
-//                         <button class="btn btn-danger">Delete</button>
-//                     </div>
-//                     </div>
-//                 </div>
+images.forEach((ele)=>{
 
-    
-//     `
-// })
+  let catg = ele.substring(ele.indexOf("?")+1);
 
-// forEach using Arrow function 
+  result += `
+        <div class="col-md-4 " >
+          <div class="card">
+           <div class="card-header">
+                <h3 class="text-capitalize">
+                   ${catg} Image
+                </h3>
+              </div>
+              <div class="card-body card-img" >
+                 <img src="${ele}" alt="Nature img" title="Nature_Img">
+              </div>
+        
+              <div class="card-footer ">
+                <button class="btn btn-primary">Edit</button>
+                <button class="btn btn-danger">Delete</button>
+              </div>
+          </div>
+      </div> 
 
-posts.forEach((post) => {
-    result +=`
-                <div class="col-md-4 mb-3" >
-                    <div class="card">
-                    <div class="card-header">
-                       
-                        <h4>${post.title}</h4>
-                    </div>
-                    <div class="card-body text-right">
-                        <p>${post.body}</p>
-                    </div>
-                    <div class="card-footer ">
-                        <button class="btn btn-primary">Edit</button>
-                        <button class="btn btn-danger">Delete</button>
-                    </div>
-                    </div>
-                </div>
-
-    
-    `
+  `
 })
 
-postsContainer.innerHTML = result;
-
-
-
-
+imgContainer.innerHTML=result;
 
